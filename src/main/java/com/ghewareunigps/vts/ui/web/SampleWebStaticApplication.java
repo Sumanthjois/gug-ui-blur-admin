469e05784933a12bdd86d2c40edf96be6d1a0ead
Deepti Kohli <deepti@ghewareunigps.com>
2017-03-10 13:12:32 +0530

    base code refactored

--- src/main/java/com/ghewareunigps/vts/ui/web/SampleWebStaticApplication.java
+++ src/main/java/com/ghewareunigps/vts/ui/web/SampleWebStaticApplication.java
@@ -14,20 +14,14 @@
  * limitations under the License.
  */
 
-package sample.web.staticcontent;
+package com.ghewareunigps.vts.ui.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SampleWebStaticApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SampleWebStaticApplication.class);
-	}
+public class SampleWebStaticApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SampleWebStaticApplication.class, args);
