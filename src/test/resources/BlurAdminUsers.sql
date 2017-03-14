a712f2a4829433bf8601c8285e805fee4bb5f0ea
Mr.Jois <joissumanth@gmail.com>
2017-03-14 22:48:20 +0530

    Update BlurAdminUsers.sql

--- src/test/resources/BlurAdminUsers.sql
+++ src/test/resources/BlurAdminUsers.sql
@@ -1,2 +1,6 @@
-create table 'BlurAdminUsers' ('id' int auto_increment primary key, 'username' varchar(300), 'email' varchar(300), 'password' varchar(300), 'role' varchar(50), 'enabled' TINYINT not null DEFAULT 1);
-select * from 'BlurAdminUsers';
+create table BlurAdminUsers (id int auto_increment primary key, username varchar(300), 
+
+email varchar(300), password varchar(300), role varchar(50), enabled TINYINT not null 
+
+DEFAULT 1);
+select * from BlurAdminUsers;
