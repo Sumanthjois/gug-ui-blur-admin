c6755fe19bb5be6dd8b7d72ca3aa40ad3fddaf33
Mr.Jois <joissumanth@gmail.com>
2017-03-14 22:39:01 +0530

    Create BlurAdminUsers.sql

---
+++ src/test/resources/BlurAdminUsers.sql
@@ -0,0 +1,2 @@
+create table 'BlurAdminUsers' ('id' int auto_increment primary key, 'username' varchar(300), 'email' varchar(300), 'password' varchar(300), 'role' varchar(50), 'enabled' TINYINT not null DEFAULT 1);
+select * from 'BlurAdminUsers';
